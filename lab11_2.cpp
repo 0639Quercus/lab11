0d67cb10ac75682fd70bb52f3d66055f5409e40f
0639Quercus <thsomsits.brie@gmail.com>
2021-12-17 16:04:55 +0700

    lab11_2.cpp completed

--- lab11_2.cpp
+++ lab11_2.cpp
@@ -9,8 +9,12 @@ int main (){
 	source.open("cheerbook.txt") ;
 	dest.open("cheerbook_copy.txt");
 	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
+	string str;
+	dest << "-------------------- BOOM ---------------------\n";
+	while (getline(source, str)) {
+		dest << str << '\n';
+	}
+	dest << "-------------------- HA!! ---------------------";
 
     source.close();
     dest.close();
